5e8bf8b2154d4b28b081e4d8a7bc924a42e32f74
Vyktho <pichonvict@cy-tech.fr>
2023-10-06 14:42:21 +0200

    extraction de la function de connection

--- playground.py
+++ playground.py
@@ -9,12 +9,18 @@ COLLECTION_NAME = 'spotify'
 # Remember to lauch mongo server before running this script
 URI = "mongodb://root:admin@localhost:27017/?authSource=admin&readPreference=primary&ssl=false&directConnection=true"
 
-def mongoimport(csv_path, db_name, coll_name, connection_uri=URI) :
+def connection(uri=URI):
+    """ Returns a connection to MongoDB
+    """
+    mongoClient = MongoClient(uri)
+    return mongoClient
+
+
+def mongoimport(csv_path, db_name, coll_name, client=connection()) :
     """ Imports a csv file at path csv_name to a mongo colection
     returns: count of the documents in the new collection
     """
-    mongoClient = MongoClient(connection_uri)
-    db = mongoClient[db_name]
+    db = client[db_name]
     collection = db[coll_name]
 
     # Get the headers from the csv file
@@ -31,6 +37,21 @@ def mongoimport(csv_path, db_name, coll_name, connection_uri=URI) :
         for field in header:
             row[field] = each[field]
         collection.insert_one(row)
+    
+    return 1
+
+# function with the genre in input that returns the list of the songs of that genre
+def get_songs(genre, client):
+    db = client[DB_NAME]
+    collection = db[COLLECTION_NAME]
+    songs = collection.find({"genre": genre})
+    # convert the result to a list
+    songs = list(songs)
+    return songs
+
+# call the function with the genre in input
+songs = get_songs("pop")
+print(songs)
 
 if __name__ == "__main__" :
     # path to csv file
@@ -41,16 +62,3 @@ if __name__ == "__main__" :
     coll_name = 'spotify'
     mongoimport(csv_path, db_name, coll_name)
 
-# function with the genre in input that returns the list of the songs of that genre
-def get_songs(genre):
-    mongoClient = MongoClient(URI)
-    db = mongoClient[DB_NAME]
-    collection = db[COLLECTION_NAME]
-    songs = collection.find({"genre": genre})
-    # convert the result to a list
-    songs = list(songs)
-    return songs
-
-# call the function with the genre in input
-songs = get_songs("pop")
-print(songs)
