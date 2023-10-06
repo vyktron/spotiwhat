a92b0b4dd696aaf9e8ab52c849f2d70494ba341c
Vyktho <pichonvict@cy-tech.fr>
2023-10-06 14:26:49 +0200

    le site marche

--- Quentin/site.py
+++ Quentin/site.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+import os 
 
-app = Flask(__name__)
+
+template_path = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(__name__, template_folder=template_path)
+
 
 @app.route('/')
 def index():
